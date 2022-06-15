c93ee762a62f82100c60127416774d7d2aeba784
Bennett Kautz <bennettkautz@gmail.com>
2022-06-15 19:09:38 -0400

    findbyID and findsteps

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -28,14 +28,30 @@ function find() { // EXERCISE A
 
 }
 
-function findById(scheme_id) { 
+async function findById(scheme_id) { 
   
-  return db('schemes as sc')
+  const rows = await db('schemes as sc')
   .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+  .where({ 'sc.scheme_id': scheme_id})
   .select('sc.scheme_name', 'st.*')
   .orderBy('st.step_number', 'asc')
-  .where({ 'sc.scheme_id': scheme_id})
   
+  const result  = {
+    scheme_id: rows[0].scheme_id,
+    scheme_name: rows[0].scheme_name,
+    steps: []
+  }
+
+  rows.forEach(row => {
+    if(row.step_id) {
+      result.steps.push({
+        scheme_id: row.step_id,
+        scheme_number: row.step_number,
+        instructions: row.instructions
+      })
+    }
+  })
+  return result
   
   
   // EXERCISE B
@@ -108,7 +124,12 @@ function findById(scheme_id) {
 
 function findSteps(scheme_id) {
   
-  return db('steps as st')
+  return db('schemes as sc')
+  .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+  .where('sc.scheme_id', scheme_id)
+  .select('step_id', 'step_number', 'instructions', 'scheme_name')
+  .orderBy('step_number')
+
   // EXERCISE C
   /*
     1C- Build a query in Knex that returns the following data.
